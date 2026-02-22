61b2240464f4eb3ecce9cf5a049241dbe7e17b1a
ksiki <j.kolyasha@gmail.com>
2026-02-22 21:58:02 +0300

    update sql_step__03_index

--- sql/03_index/031_create_idx_fact_orders_user_id.sql
+++
@@ -1,2 +0,0 @@
-create index if not exists idx_fact_orders_user_id
-on mart.fact_orders (user_id);
--- sql/03_index/032_create_idx_fact_orders_user_id_and_status.sql
+++
@@ -1,2 +0,0 @@
-CREATE INDEX IF NOT EXISTS idx_fact_orders_user_id_and_status
-ON mart.fact_orders (user_id, status);
